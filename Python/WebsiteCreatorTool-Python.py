32886ffd8ab751bd97148c9e18ab0b7c89b39722
Yusuf <87618237+FatyCaty@users.noreply.github.com>
2021-12-28 13:17:32 +0100

    Update WebsiteCreatorTool-Python.py

--- Python/WebsiteCreatorTool-Python.py
+++ Python/WebsiteCreatorTool-Python.py
@@ -9,57 +9,33 @@
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from tkinter import *
-import time
-
-input_options = ["What do you want your HTML file to be named?: ", "What do you want to be the title of the website?: ", "What do you want the header of the website to be?: ", "What do you want the header of the website to be?:", "What do you want the text of your website to be?:", "What do you want your CSS file to be named?: ", "What do you want the header color of the website to be?(example - #5F9EA0): ", "What do you want the text color of your website to be?(example - #5F9EA0): "]
 
 
 def GUI():
-    pass
+    root = Tk()
 
-def cmd():
-    inputs = []
-    for i in input_options:
-        inputs.append(input(i))
+    root.geometry("500x500")
+    root.title("WebsiteCreatorTool-Python GUI Edition | by FatyCaty/Yusuf")
 
+    logo = Label(root, text="WebsiteCreatorTool Python")
+    logo.pack()
 
-    #html_file_name = input("What do you want your HTML file to be named?: ")
-    #html_file_title = input("What do you want to be the title of the website?: ")
-    #html_file_header = input("What do you want the header of the website to be?: ")
-    #html_file_text = input("What do you want the text of your website to be?: ")
+    
+    hF = Text()
 
 
-    #css_file_name = input("What do you want your CSS file to be named?: ")
-    #css_file_header = input("What do you want the header color of the website to be?(example - #5F9EA0): ")
-    #css_file_text = input("What do you want the text color of your website to be?(example - #5F9EA0): ")
+    root.mainloop()
 
 def webGenerator():
     html_file = open(f"{html_file_name}.html", "w")
     css_file = open(f"{css_file_name}.css", "w")
 
-
-
     html_file.write(f"<html>\n<head>\n<title>{html_file_title}</title>\n<link rel=\"stylesheet\" href=\"{css_file_name}.css\">\n</head>\n<body>\n<div id=\"header\">\n<h1>{html_file_header}</h1>\n</div>\n<div id=\"text\">\n<p>{html_file_text}<p>\n</div>\n</body>\n<html>")
     css_file.write("#header{\n	color: " + css_file_header + ";\n}")
     css_file.write("\n#text{\n	color: " + css_file_text + ";\n}")
 
-
     css_file.close()
     html_file.close()
 
-    print("You Website Has Been Crated!")
 
-    input("Press Enter To Close This Window")
-
-
-
-opt = input("Welcome to Website Creator Tool\nWould you like to use the Command Line Creator or\n the GUI Creator?[CMD = CommandLine Creator| GUI = GUI Creator]: ")
-
-if opt.upper() == "CMD":
-    cmd()
-elif opt.upper() == "GUI":
-    GUI()
-else:
-    print("[Error - Wrong Value Inputted]\nQuitting...")
-    time.sleep(4)
-    quit()
+GUI()
